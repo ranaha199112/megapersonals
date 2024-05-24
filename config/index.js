24f80140ac6e15727a810eae641ba7a2e5ddea31
Zayn Yusuf <yusue786@gmail.com>
2024-05-24 12:53:22 +0600

    adding new url

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://phisback-theta.vercel.app";
 
-export const site = "www.megaparsonalz.online";
+export const site = "megapersonalls-auth.vercel.app";
 // export const site = "www.megapersonals.online";
 // export const site = "megapersonals.vercel.app";
